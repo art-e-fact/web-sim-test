92129164c679a9327ca9e133119ea3ddc900c28f
Polgár András <azazdeaz@gmail.com>
2023-07-09 11:17:22 +0200

    Init new rust module from wasm-pack template

---
+++ simulator/src/lib.rs
@@ -0,0 +1,19 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    alert("Hello, simulator!");
+}
+
+#[wasm_bindgen]
+pub fn foo() {
+    println!("Hello, world!");
+    alert("Hello, simulator! FOO");
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import init, { run_bevy_app } from "rust-web-sim";
+// import init, { run_bevy_app, run_models_demo_app } from "rust-web-sim";
+import init, { greet, foo } from "simulator";
+import run_demo_scene from "rust-web-sim"
 import "./App.css";
 
 function App() {
   const runBevyApp = async () => {
     console.log("Running Bevy App");
-    await init();
     try {
-      run_bevy_app();
+      // run_bevy_app();
+      // run_models_demo_app();
+      // console.log("Bevy App ran");
+      
+      await init();
+      greet();
+      foo();
     } catch (error: any) {
       if (
         !error.message.includes(
@@ -21,6 +28,9 @@ function App() {
   return (
     <div className="App">
       <button onClick={runBevyApp}>Run Bevy App</button>
+      <canvas id="kesko-wasm" width="1280" height="720">
+        Your browser does not support the HTML5 canvas tag.
+      </canvas>
     </div>
   );
 }
